5026e024627e6a5c07db6c4e87f03e55c7036c54
pfpetrowski <pfpetrowski@gmail.com>
2023-02-18 12:12:51 -0600

    update examples and specify the package fucntion calls are coming from using ::

--- R/buildfreqtable.R
+++ R/buildfreqtable.R
@@ -11,23 +11,25 @@
 #' @details This function groups the data by the specified subgroup and trait, and counts the occurrences for each combination. It then reshapes the data into a frequency table.
 #'
 #' @examples
-#' buildfreqtable(data = mydata, trait = "weight", subgroup = "SnellCategory")
+#' mydata <- data.frame("Groups" = rep(c("A", "B", "C", "D"), 10),
+#'                     "Scores" = round(runif(40, 0, 5)))
+#' buildfreqtable(data = mydata, trait = "Scores", subgroup = "Groups")
 #'
 #' @export
 buildfreqtable <- function(data, trait, subgroup){
   # groups the data by subgroup and trait and counts the occurrences for each combination
   # column name of the count column is changed to the specified trait
-  counts <- data %>% group_by(across(all_of(c(subgroup, trait)))) %>% count()
+  counts <- data %>% dplyr::group_by(across(all_of(c(subgroup, trait)))) %>% count()
   colnames(counts)[2] <- trait
   
   # reshapes the data into frequency table with the specified subgroup as index, 
   # specified trait as column names and count as values.
   # combinations of subgroup and trait that do not appear in the original data are still represented
   # with a count of 0 using values_fill=0
-  freqtable <- counts %>% pivot_wider(id_cols = subgroup,
-                                      names_from = trait,
-                                      values_from = n,
-                                      values_fill = 0)
+  freqtable <- counts %>% tidyr::pivot_wider(id_cols = subgroup,
+                                             names_from = trait,
+                                             values_from = n,
+                                             values_fill = 0)
   # reshapes the data into frequency table with HerdYear as index, 
   # trait as column names and count as values.
   # combinations of HerdYear and trait that do not appear in the original data are still represented
@@ -35,8 +37,8 @@ buildfreqtable <- function(data, trait, subgroup){
   
   
   freqtable <- as.data.frame(freqtable) %>%
-    column_to_rownames(subgroup) %>%
-    select(all_of(as.character(sort(unique(data[, trait])))))
+    tibble::column_to_rownames(subgroup) %>%
+    dplyr::select(all_of(as.character(sort(unique(data[, trait])))))
   #Ensure that columns are sorted in order of ascending score
   #And drop the HerdYear column
   
