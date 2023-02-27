d86d008d2a1d232a3e330664ed4ed7fcfbfe8ccf
pfpetrowski <pfpetrowski@gmail.com>
2023-02-26 21:35:46 -0600

    new 'order' argument for buildfreqtable

--- R/buildfreqtable.R
+++ R/buildfreqtable.R
@@ -5,6 +5,7 @@
 #' @param data A data frame containing the raw data
 #' @param trait A character string specifying the trait to be analyzed
 #' @param subgroup A character string specifying the column containing the grouping variable
+#' @param order A character vector specifying the order in which the categories of the trait should be placed
 #'
 #' @return A frequency table with the specified subgroup as the rownames, the scores of the specified trait as column names, and count as values
 #'
@@ -22,7 +23,7 @@
 #' @importFrom tibble column_to_rownames
 #'
 #' @export
-buildfreqtable <- function(data, trait, subgroup){
+buildfreqtable <- function(data, trait, subgroup, order){
   # groups the data by subgroup and trait and counts the occurrences for each combination
   # column name of the count column is changed to the specified trait
   counts <- data %>% dplyr::group_by(dplyr::across(tidyselect::all_of(c(subgroup, trait)))) %>% dplyr::count()
@@ -36,19 +37,32 @@ buildfreqtable <- function(data, trait, subgroup){
                                              names_from = trait,
                                              values_from = "n",
                                              values_fill = 0)
-  # reshapes the data into frequency table with HerdYear as index, 
-  # trait as column names and count as values.
-  # combinations of HerdYear and trait that do not appear in the original data are still represented
-  # with a count of 0 using values_fill=0
   
+  if (missing(order)){ # Assuming numeric scores if 
+    freqtable <- as.data.frame(freqtable) %>%
+      tibble::column_to_rownames(subgroup) %>%
+      dplyr::select(tidyselect::all_of(as.character(sort(unique(data[, trait])))))
+    #Ensure that columns are sorted in order of ascending score
+    #And drop the HerdYear column
+  }
   
-  freqtable <- as.data.frame(freqtable) %>%
-    tibble::column_to_rownames(subgroup) %>%
-    dplyr::select(tidyselect::all_of(as.character(sort(unique(data[, trait])))))
-  #Ensure that columns are sorted in order of ascending score
-  #And drop the HerdYear column
+  else{
+    
+    if (all(colnames(freqtable)[-1] %in% order)){
+      freqtable <- as.data.frame(freqtable) %>%
+        tibble::column_to_rownames(subgroup) %>%
+        dplyr::select(tidyselect::all_of(order))
+      # sort columns in the specified order
+      # And drop the HerdYear column
+    }
+    
+    else{
+      stop("Not all categories in the trait are specified in the order")
+    }
+    
+  }
   
-  if (ncol(data) <= 2){
+  if (n_distinct(data[[trait]]) <= 2){
     warning("A frequency table has been produced, but the trait does not have 3 or more categories
             and snell will fail if this table is passed.")
   }
